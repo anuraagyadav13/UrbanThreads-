c36294430720c165441cb7778ce4f66c47d1a0b4
anuraagyadav13 <anutechx2707@gmail.com>
2025-08-29 22:53:27 +0530

    contact info updated

--- client/src/ui/Footer.jsx
+++ client/src/ui/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
 	return (
 		<footer className="grid grid-cols-1 md:(grid-cols-3) border-t border-gray-300 bg-gray-100 dark:(bg-zinc-900 border-zinc-800) px-4">
 			<div className="m-4 sm:m-6 flex-1">
-				<h2 className="text-4xl text-center md:text-left mb-4">BRAND</h2>
+				<h2 className="text-4xl text-center md:text-left mb-4">Urban Threads</h2>
 				<p className="text-justify text-gray-700 dark:text-zinc-300">Eiusmod duis reprehenderit quis cillum nisi anim consectetur occaecat cupidatat anim incididunt aliqua eiusmod ad consectetur in ut cupidatat proident dolore aute irure enim in in ut adipisicing in do est.</p>
 				<ul className="flex mt-6 justify-center md:justify-start space-x-4">
 					<li>
@@ -19,10 +19,10 @@ export default function Footer() {
 						<Link to="#"><Twitter /></Link>
 					</li>
 					<li>
-						<a href="https://github.com/nimone/Fashion-Store"><GitHub /></a>
+						<a href="https://github.com/anuraagyadav13/UrbanThreads-"><GitHub /></a>
 					</li>
 					<li>
-						<a href="https://nimo.pages.dev"><Globe /></a>
+						<a href="https://anurag-yadav.pages.dev"><Globe /></a>
 					</li>
 				</ul>
 			</div>			
@@ -60,26 +60,26 @@ export default function Footer() {
 				<ul className="space-y-3 text-gray-700 dark:text-zinc-300">
 					<li className="flex items-center">
 						<MapPin className="w-5 h-5 mr-2" />
-						<span>221b Baker St, London NW1 6XE, UK</span>
+						<span>E-904,The Vasundhara Grand , Sector 15,Vasundhara ,Ghaziabad -201010</span>
 					</li>					
 					<li className="flex items-center">
 						<Phone className="w-5 h-5 mr-2" />
-						<span>+1234-567-890</span>
+						<span>+91 9267903565 </span>
 					</li>					
 					<li className="flex items-center">
 						<Mail className="w-5 h-5 mr-2" />
-						<a href="mailto:nimogha@gmail.com" target="_blank">
-							nimogha@gmail.com
+						<a href="mailto:anutechx2707@gmail.com" target="_blank">
+						anutechx2707@gmail.com
 						</a>
 					</li>
 					<li className="flex items-center">
 						<GitHub className="w-5 h-5 mr-2" />
-						<a href="https://github.com/nimone/Fashion-Store" target="_blank">
-							/Fashion-Store
+						<a href="https://github.com/anuraagyadav13/UrbanThreads-" target="_blank">
+							/Urban-Threads
 						</a>
 						<span className='mx-1'>built by</span>
-						<a href="https://nimo.pages.dev" target="_blank" className='border-b-2 border-green-500'>
-							nimo
+						<a href="https://anurag-yadav.pages.dev" target="_blank" className='border-b-2 border-green-500'>
+							Anurag Yadav 	
 						</a>
 					</li>
 				</ul>
